4bef861bb2b680bf79480b796bf86a094863c4d8
Marinko <marinkospasojevic@gmail.com>
2020-09-07 13:39:20 +0200

    Blazor WebAssembly SignalR source code

---
+++ End/BlazorProducts.Client/BlazorProducts.Client/Pages/SignalRCharts.razor.cs
@@ -0,0 +1,59 @@
+﻿using BlazorProducts.Client.HttpRepository;
+using Entities.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Client.Pages
+{
+	public partial class SignalRCharts : IDisposable
+	{
+		private HubConnection _hubConnection;
+		private List<ChartDto> _data;
+
+		public List<ChartDto> Data = new List<ChartDto>();
+		[Inject]
+		public IProductHttpRepository Repo { get; set; }
+
+		protected async override Task OnInitializedAsync()
+		{
+			await StartHubConnection();
+
+			AddTransferChartDataListener();
+
+			await Repo.CallChartEndpoint();
+		}
+
+		private async Task StartHubConnection()
+		{
+			_hubConnection = new HubConnectionBuilder()
+							.WithUrl("https://localhost:5011/chart")
+							.Build();
+
+			await _hubConnection.StartAsync();
+			if (_hubConnection.State == HubConnectionState.Connected)
+				Console.WriteLine("connection started");
+		}
+
+		private void AddTransferChartDataListener()
+		{
+			_hubConnection.On<List<ChartDto>>("transferchartdata", (_data) =>
+			{
+				foreach (var item in _data)
+				{
+					Console.WriteLine($"Label: {item.Label}, Value: {item.Value}");
+				}
+
+				Data = _data;
+				StateHasChanged();
+			});
+		}
+
+		public void Dispose()
+		{
+			_hubConnection.DisposeAsync();
+		}
+	}
+}
---
+++ End/BlazorProducts.Server/BlazorProducts.Server/HubConfig/ChartHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorProducts.Server.HubConfig
+{
+	public class ChartHub : Hub
+	{
+	}
+}
